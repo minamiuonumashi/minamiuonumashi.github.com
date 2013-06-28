05b6b746c640beb29e8bc479e27842ef6949fbc4
Satoshi Shibata <satosh@ud-c.com>
2013-06-28 17:16:51 +0900

    アイコン色変更テスト

--- js/setting.js
+++ js/setting.js
@@ -17,9 +17,9 @@ OpenSpending.Styles = OpenSpending.Styles || {};
 
 OpenSpending.Styles.Cofog = {
 
-  /* yokohama cofog */
-  '1': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-  '1-1': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+  /* chofu cofog */
+  '1': { icon: 'icons/helping-others.svg', color: '#0099FF', bcolor: '#935B3B' },
+  '1-1': { icon: 'icons/helping-others.svg', color: '#0099FF', bcolor: '#935B3B' },
   '2': { icon: 'icons/schools.svg', color: '#C75746', bcolor: '#0AB971' },
   '2-1': { icon: 'icons/pre-school.svg', color: '#C75746', bcolor: '#0AB971' },
   '2-2': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#0AB971' },
