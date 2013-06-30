9e8eee62a5e338f97a1704dfcb3d7016e4dc9221
Satoshi Shibata <satosh@ud-c.com>
2013-06-30 16:54:56 +0900

    設定ファイルテスト

--- js/setting.js
+++ js/setting.js
@@ -16,6 +16,10 @@ OpenSpending.year = '2012';
 OpenSpending.Styles = OpenSpending.Styles || {};
 
 OpenSpending.Styles.Cofog = {
+	
+  /* yokohama cofog */
+  '1': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+  '1-1': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' }
   /* koganei cofog */
   '1': { icon: 'icons/legislative.svg', color: '#C75746', bcolor: '#935B3B' }, // 議会費
   '101': { icon: 'icons/legislative.svg', color: '#C75746', bcolor: '#935B3B' }, // 議会費 議会費
