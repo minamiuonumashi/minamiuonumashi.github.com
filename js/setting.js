850e11be0863c483da3dfa6bd841d25729fbdf30
Satoshi Shibata <satosh923@i.softbank.jp>
2013-07-21 01:01:58 +0900

    change icon color

--- js/setting.js
+++ js/setting.js
@@ -18,19 +18,19 @@ OpenSpending.Styles = OpenSpending.Styles || {};
 OpenSpending.Styles.Cofog = {
 
   /* chofu cofog */
-  '1': { icon: 'icons/legislative.svg', color: '#0099FF', bcolor: '#935B3B' }, // 議会費
-  '2': { icon: 'icons/publicaffairs.svg', color: '#0099FF', bcolor: '#0AB971' }, // 総務費
-  '3': { icon: 'icons/family.svg', color: '#0099FF', bcolor: '#4E6D00' }, // 民生費
-  '4': { icon: 'icons/health.svg', color: '#0099FF', bcolor: '#D33673' }, // 衛生費
-  '5': { icon: 'icons/environment.svg', color: '#0099FF', bcolor: '#2A3A03' }, // 労働費
-  '6': { icon: 'icons/civilian-action.svg', color: '#0099FF', bcolor: '#EC2406' }, // 農業費
-  '7': { icon: 'icons/traffic-watersup.svg', color: '#0099FF', bcolor: '#938626' }, // 商工費
-  '8': { icon: 'icons/coal.svg', color: '#0099FF', bcolor: '#0099FF' }, // 土木費
-  '9': { icon: 'icons/order-safety.svg', color: '#0099FF', bcolor: '#D33673' }, // 消防費
-  '10': { icon: 'icons/schools.svg', color: '#0099FF', bcolor: '#790586' }, // 教育費
-  '11': { icon: 'icons/economic-aid.svg', color: '#0099FF', bcolor: '#D36B3B' }, // 公債費
-  '12': { icon: 'icons/financial-admin.svg', color: '#0099FF', bcolor: '#C35B4B' }, // 予備費
-  '13': { icon: 'icons/construction.svg', color: '#0099FF', bcolor: '#935B3B' }, // 災害復旧費
+  '1': { icon: 'icons/legislative.svg', color: '#5BAC0B', bcolor: '#935B3B' }, // 議会費
+  '2': { icon: 'icons/publicaffairs.svg', color: '#5BAC0B', bcolor: '#0AB971' }, // 総務費
+  '3': { icon: 'icons/family.svg', color: '#5BAC0B', bcolor: '#4E6D00' }, // 民生費
+  '4': { icon: 'icons/health.svg', color: '#5BAC0B', bcolor: '#D33673' }, // 衛生費
+  '5': { icon: 'icons/environment.svg', color: '#5BAC0B', bcolor: '#2A3A03' }, // 労働費
+  '6': { icon: 'icons/civilian-action.svg', color: '#5BAC0B', bcolor: '#EC2406' }, // 農業費
+  '7': { icon: 'icons/traffic-watersup.svg', color: '#5BAC0B', bcolor: '#938626' }, // 商工費
+  '8': { icon: 'icons/coal.svg', color: '#5BAC0B', bcolor: '#0099FF' }, // 土木費
+  '9': { icon: 'icons/order-safety.svg', color: '#5BAC0B', bcolor: '#D33673' }, // 消防費
+  '10': { icon: 'icons/schools.svg', color: '#5BAC0B', bcolor: '#790586' }, // 教育費
+  '11': { icon: 'icons/economic-aid.svg', color: '#5BAC0B', bcolor: '#D36B3B' }, // 公債費
+  '12': { icon: 'icons/financial-admin.svg', color: '#5BAC0B', bcolor: '#C35B4B' }, // 予備費
+  '13': { icon: 'icons/construction.svg', color: '#5BAC0B', bcolor: '#935B3B' }, // 災害復旧費
   };
   
   
